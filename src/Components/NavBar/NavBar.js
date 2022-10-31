66bd7d62aac30339cd2fcd53bd7bc2f060ac7353
ElBrazilian <nathan.schiffmacher@gmail.com>
2022-10-31 11:09:25 +0100

    Navbar is now fixed

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -6,17 +6,20 @@ import './NavBar.css';
 
 function NavBar(){
 
-    return (<header>
-        <div className="nav_logo">
-            
-        </div>
-        <nav>
-            <NavLink href="who">Qui sommes-nous ?</NavLink>
-            <NavLink>Nos actions</NavLink>
-            <NavLink>Actualités</NavLink>
-            <NavLink>Nous contacter</NavLink>
-        </nav>
-    </header>);
+    return (<div className="navbar">
+        <header>
+            <div className="nav_logo">
+                
+            </div>
+            <nav>
+                <NavLink href="who">Qui sommes-nous ?</NavLink>
+                <NavLink>Nos actions</NavLink>
+                <NavLink>Actualités</NavLink>
+                <NavLink>Nous contacter</NavLink>
+            </nav>
+        </header>
+        <div className="header_spacer"></div>
+    </div>);
 }
 
 export default NavBar;

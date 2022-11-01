0d98df7ffeed842143fd9599349d46fb391b55fb
ElBrazilian <nathan.schiffmacher@gmail.com>
2022-11-01 22:16:53 +0100

    Done with the contact section on mobile and tablets

--- src/Components/ContactsSection/ContactsSection.js
+++ src/Components/ContactsSection/ContactsSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 import TextWithIcon from '../TextWithIcon/TextWithIcon';
@@ -9,9 +9,17 @@ function ContactsSection() {
   const navigate = useNavigate();
   const contact_click = (e) => {
     e.preventDefault();
-    console.log('??')
     navigate('/contact');
   } 
+
+  
+  const [is_phone] = useState(
+    window.innerWidth < 768 ? true : false
+  );
+
+  const facebook_username = "@asso.isaelles";
+  const twitter_username  = "@isaelles" + (is_phone ? "\u00A0".repeat(9) : "");
+  const linkedin_username = "@ISAElles" + (is_phone ? "\u00A0".repeat(8) : "");
 
   return (
     <div className="contacts_section">
@@ -19,9 +27,9 @@ function ContactsSection() {
             <img src="/images/logo_isaelles.svg" alt="isaelles logo" />
         </div>
         <div className="contacts">
-            <TextWithIcon href="https://www.facebook.com/asso.isaelles" icon="/images/contacts_logo/facebook.png" className="contact contact_facebook">@asso.isaelles</TextWithIcon>
-            <TextWithIcon href="https://twitter.com/isaelles" icon="/images/contacts_logo/twitter.svg" className="contact contact_twitter">@isaelles</TextWithIcon>
-            <TextWithIcon href="https://www.linkedin.com/company/isaelles" icon="/images/contacts_logo/linkedin.png" className="contact contact_linkedin">@ISAElles</TextWithIcon>
+            <TextWithIcon href="https://www.facebook.com/asso.isaelles" icon="/images/contacts_logo/facebook.png" className="contact contact_facebook">{ facebook_username }</TextWithIcon>
+            <TextWithIcon href="https://twitter.com/isaelles" icon="/images/contacts_logo/twitter.svg" className="contact contact_twitter">{ twitter_username }</TextWithIcon>
+            <TextWithIcon href="https://www.linkedin.com/company/isaelles" icon="/images/contacts_logo/linkedin.png" className="contact contact_linkedin">{ linkedin_username }</TextWithIcon>
         </div>
         <TextWithIcon icon="/images/contacts_logo/pinpoint.png" className="contact contact_adress">4 Avenue Edouard Belin, 31400 Toulouse, France</TextWithIcon>
         

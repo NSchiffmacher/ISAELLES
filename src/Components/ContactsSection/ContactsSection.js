fc31cb0d62e650473cca1823aa44e786800a0697
ElBrazilian <nathan.schiffmacher@gmail.com>
2022-11-01 14:28:10 +0100

    Setting up the contact section

---
+++ src/Components/ContactsSection/ContactsSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './ContactsSection.css';
+
+function ContactsSection() {
+  return (
+    <div className="contacts_section">ContactsSection</div>
+  )
+}
+
+export default ContactsSection

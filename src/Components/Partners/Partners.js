d8eb33a85334272c274d8d8bd7c010443cc50415
ElBrazilian <nathan.schiffmacher@gmail.com>
2022-11-09 21:07:47 +0100

    Added the ability to set the intersectionRatio needed for the useAppearTransition Hook

--- src/Components/Partners/Partners.js
+++ src/Components/Partners/Partners.js
@@ -9,8 +9,8 @@ import './Partners.css';
 function Partners(props) {
     // Code handling the linked appearance of the != logos
     const base_logos_style = {
-        transform: 'translateY(60px)',
-        opacity: '0.1',
+        transform: 'translateY(250px)',
+        opacity: '0',
         transition: 'transform 0.4s ease-out, opacity 0.3s ease-out'
     };
     let [logos_style, set_logos_style] = useState(base_logos_style);
@@ -21,7 +21,7 @@ function Partners(props) {
             transform: 'translateY(0px)',
             opacity: '1'
         });
-    });
+    }, {intersectionRatio: 0.2});
 
 
     return (
@@ -33,7 +33,7 @@ function Partners(props) {
                         { src: '/images/logo_capgemini.png', alt: 'logo capgemini' },
                         { src: '/images/logo_fondation.png', alt: 'logo fondation' },
                         { src: '/images/logo_ose.jpg',       alt: 'logo ose'}
-                    ].map(({src, alt}, index) => (<div className="image_container" style={{...logos_style, transitionDelay: (base_transition_delay + index * transition_delay) + 's'}}><div className="partner_logo"><img className="scale-2prc" src={ src } alt={ alt } /></div></div>))}
+                    ].map(({src, alt}, index) => (<div key={ "logo_partners_" + index} className="image_container" style={{...logos_style, transitionDelay: (base_transition_delay + index * transition_delay) + 's'}}><div className="partner_logo"><img className="scale-2prc" src={ src } alt={ alt } /></div></div>))}
                 </div>
             </div>
             <div className="etablissements_partenaires">
--- src/util/useAppearTransition.js
+++ src/util/useAppearTransition.js
@@ -1,16 +1,18 @@
 import { useEffect, useState } from 'react'
 import { useInView } from 'react-intersection-observer';
 
-const useAppearTransition = (callback) => {
-    const { ref, inView } = useInView({});
+const useAppearTransition = (callback, options = {}) => {
+    const intersectionRatio = options.intersectionRatio | 0;
+
+    const { ref, inView, entry } = useInView({});
     const [alreadyEntered, setAlreadyEntered] = useState(false);
 
     useEffect(() => {
-        if (inView && !alreadyEntered) {
+        if (inView && !alreadyEntered && entry.intersectionRatio >= intersectionRatio) {
             callback();
             setAlreadyEntered(true);
         }
-    }, [inView, alreadyEntered, callback]);
+    }, [inView, alreadyEntered, callback, entry, intersectionRatio]);
 
     return ref;
 }

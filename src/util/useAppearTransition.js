d6792b7fe703ccaa1ae958e79cd7ef5e3b7c7ff6
ElBrazilian <nathan.schiffmacher@gmail.com>
2022-11-09 21:33:27 +0100

    Fix not working on chrome ?

--- src/util/useAppearTransition.js
+++ src/util/useAppearTransition.js
@@ -2,16 +2,12 @@ import { useState } from 'react'
 import { useInView } from 'react-intersection-observer';
 
 const useAppearTransition = (callback, options = {}) => {
-    const threshold = options.threshold | 0;
-
     const [alreadyEntered, setAlreadyEntered] = useState(false);
 
     const { ref } = useInView({
-        threshold: threshold,
+        ...options, 
         onChange: (cur_inView, cur_entry) => {
-            console.log(document.readyState)
             if (!alreadyEntered && cur_inView && document.readyState === 'complete') {
-                console.log('callback called')
                 callback();
                 setAlreadyEntered(true);
             }

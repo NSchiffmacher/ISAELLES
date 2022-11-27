251a26809bbb834fd47b0afeaa5dd0673fe925ed
ElBrazilian <nathan.schiffmacher@gmail.com>
2022-11-27 19:35:37 +0100

    [TEST] Implemented a way to wait for the loading of the page to launch the animations

--- src/util/useAppearTransition.js
+++ src/util/useAppearTransition.js
@@ -3,19 +3,45 @@ import { useInView } from 'react-intersection-observer';
 
 const useAppearTransition = (callback, options = {}) => {
     const [alreadyEntered, setAlreadyEntered] = useState(false);
+    const wait_for_load = options.wait_for_load ?? false;
 
     const { inView, ref } = useInView({
         ...options, 
     });
 
+    function executeCallback(){
+        callback();
+        setAlreadyEntered(true);
+    }
+    function isPossible(){
+        return (!(wait_for_load && !(document.readyState === 'complete')));
+    }
+
     useEffect(() => {
-        console.log('.')
+        let run_animation = !alreadyEntered && inView;
+        let set_interal_id = null;
+        
+        // console.log(!alreadyEntered && inView, !(wait_for_load && !(document.readyState === 'complete')))
         // if (!alreadyEntered && inView && document.readyState === 'complete') {
-        if (!alreadyEntered && inView   ) {
-            callback();
-            setAlreadyEntered(true);
+        console.log('hey', run_animation, isPossible())
+        if (run_animation) {
+            if (isPossible()) {
+                executeCallback();
+            } else if (set_interal_id == null) {
+                console.log("set interval")
+                set_interal_id = setInterval(() => {
+                    if (isPossible){
+                        executeCallback();
+                        clearInterval(set_interal_id);
+                        console.log('clear  ')
+                    } else {
+                        console.log('wait')
+                    }   
+                }, 1);
+            }
+            console.log('duh    ')
         }
-    }, [alreadyEntered, inView, callback, ref])
+    }, [alreadyEntered, inView, callback, ref, wait_for_load])
 
     return ref;
 }

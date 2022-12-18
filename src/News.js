32600f7ec5436d5e8da06fa71770cf41d5266b1b
ElBrazilian <nathan.schiffmacher@gmail.com>
2022-12-18 16:01:16 +0100

    Worked on the news page

---
+++ src/News.js
@@ -0,0 +1,75 @@
+const NEWS = [
+    {
+        title       : "Conférence STEM leaders NASA",
+        place       : "ISAE-SUPAERO",
+        date        : "2 juin 2022",
+        caps_title  : true,
+        caps_sub    : true,
+        images      : [],
+        content     : <p>Le jeudi 2 juin 2022 s'est tenu la conférence <b>STEM Connections: Meeting with 4 STEM Leaders</b> à l'ISAE-SUPAERO. Des étudiantes en prépa du lycée Pierre De Fermat ainsi que du lycée Déodat de Séverac ont pu assister à cette conférence. Elles ont eu l'opporunité de rencontrer Christyl Johnson (NASA), John Bolten (NASA), Teri Cochrane (Global Sustainable Health Institute) and Ines Belgacem (ESA).</p>
+    },
+    {
+        title       : "Stand Airexpo",
+        place       : "Aérodrome de Muret-Lherme",
+        date        : "14 mai 2022",
+        caps_title  : true,
+        caps_sub    : true,
+        images      : [],
+        content     : <p>blabla</p>
+    },
+    {
+        title       : "Journée campus au féminin lycée",
+        place       : "ISAE-SUPAERO",
+        date        : "10 mars 2022",
+        caps_title  : true,
+        caps_sub    : true,
+        images      : [],
+        content     : <p>Retour en images sur la Journée Campus au Féminin qui a réuni jeudi 10 mars plus de 150 lycéennes des environs à l’ISAE-SUPAERO! Les bénévoles ISAELLES et du programme OSE de la Fondation ISAE-SUPAERO ont fait visiter le campus aux lycéennes participant au projet. 
+            <ul>
+                <li>Au programme : visite de l’innovspace du campus, présentations de parcours avec l’Association Femmes Ingénieures, essais en simulateur de vol et découverte de l’exploration martienne avec le Club MARS ISAE-SUPAERO!</li>
+            </ul>
+        </p>
+    },
+    {
+        title       : "Table Ronde avec Dassault Aviation",
+        place       : "ISAE-SUPAERO",
+        date        : "9 mars 2022",
+        caps_title  : true,
+        caps_sub    : true,
+        images      : [],
+        content     : <p>Le 2 mars a eu lieu une table ronde sur les Enjeux de la féminisation des métiers techniques aux côtés de Christine Ricouart, Sylvie Gobbatti et Florie Vidal de St-Jean, 3 alumni d'ISAE-SUPAERO aujourd'hui chez Dassault Aviation.</p>
+    },
+    {
+        title       : "Semaine de l'égalité",
+        place       : "ISAE-SUPAERO",
+        date        : "8 mars 2022",
+        caps_title  : true,
+        caps_sub    : true,
+        images      : [],
+        content     : <p>Le 8 mars, en cette journée internationale des femmes, l'équipe de OSE ainsi que le bureau d'ISAELLES ont eu le plaisir d'accueillir M. le Préfet Préfet de la région Occitanie, Préfet de la Haute-Garonne, les déléguées régionales & départementales aux droits des femmes Isabelle le Dréau & Émilie Provensal ainsi que Nathalie Vitrat, directrice adjointe de la DDETS!<br/>
+        <br/><a href="https://www.isae-supaero.fr/fr/actualites/retour-sur-la-semaine-de-l-egalite-avec-ose-l-isae-supaero">Plus d'info</a>
+        </p>
+    },
+    {
+        title       : "Distribution de protections hygiéniques",
+        place       : "ISAE-SUPAERO",
+        date        : "8 mars 2022",
+        caps_title  : true,
+        caps_sub    : true,
+        images      : [],
+        content     : <p>Le 8 mars a eu lieu la distribution de protections hygiéniques auprès des étudiantes de l'ISAE-SUPAERO. Chaque année a lieu 2 distributions de protections offertes par l'école. ISAELLES permet d'être le lien entre les étudiantes et l'administration de SUPAERO afin de réduire la précarité menstruelle.</p>
+    },
+    {
+        title       : "Journée campus au féminin collège",
+        place       : "ISAE-SUPAERO",
+        date        : "3 février 2022",
+        caps_title  : true,
+        caps_sub    : true,
+        images      : [],
+        content     : <p>Retour en images sur la Journée Campus au Féminin qui faisait son grand retour en présentiel ce jeudi 3 février sur le campus de l'Institut Supérieur de l'Aéronautique et de l'Espace ! 
+        ISAELLES se joignait au programme OSE de la Fondation ISAE-SUPAERO pour accueillir 200 collégiennes des environs et leur faire découvrir le parcours de femmes ingénieures et celui qui pourrait être le leur!
+        </p>
+    },
+]
+
+export default NEWS;
--- src/Pages/NewsPage/NewsPage.js
+++ src/Pages/NewsPage/NewsPage.js
@@ -4,12 +4,26 @@ import NavBar from '../../Components/NavBar/NavBar';
 import Subtitle from '../../Components/Subtitle/Subtitle';
 import ScrollToTop from '../../Components/ScrollToTop/ScrollToTop';
 
+import Article from '../../Components/Article/Article';
+import NEWS from '../../News';
+
 // import useIsPhone from '../../util/useIsPhone';
 
 import './NewsPage.css';
 
 function NewsPage() {
     // const is_phone = useIsPhone();
+    const articles = NEWS.map(data => 
+        <Article 
+            title  = { data.caps_title ? data.title.toUpperCase() : data.caps_title   }
+            place  = { data.caps_sub   ? data.place.toUpperCase() : data.place        }
+            date   = { data.caps_sub   ? data.date.toUpperCase()  : data.date        }
+            images = { data.images  }>
+
+            { data.content }
+
+        </Article>
+    );
 
 
     return (
@@ -18,6 +32,12 @@ function NewsPage() {
             
             <NavBar />
             <Subtitle>Actualités</Subtitle>
+
+            <div className="page_wrap">
+                {articles}
+            </div>
+
+
         </div>
     );
 }
